2d374b9cae69a117ef02cf3bbb40d7d912dd674d
Šárka Klauberová <sarkaa.novakova@seznam.cz>
2023-11-18 19:47:46 +0100

    admin zobrazení listu

--- src/pages/AdminPage/index.jsx
+++ src/pages/AdminPage/index.jsx
@@ -1,3 +1,38 @@
+import './style.css';
+import { useEffect, useState } from 'react';
+
 export const AdminPage = () => {
-  return <h1>Admin</h1>;
+  const [orders, setOrders] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(
+        'http://localhost:4000/api/poptavky?filter=phase:eq:new',
+      );
+      const data = await response.json();
+      setOrders(data.result);
+    };
+    fetchData();
+  }, []);
+  console.log(orders);
+
+  return (
+    <div className="container">
+      <h1>Admin: Andulka</h1>
+      <div className="list">
+        {orders.map((item) => (
+          <ul className="list-item">
+            <h3>Poptávka č.{item.id}</h3>
+            <li>Počet nocí:{item.nights}</li>
+            <li>Počet osob: {item.persons}</li>
+            <li>Domácí mazlíček: {item.pets}</li>
+            <li>Děti: {item.children}</li>
+            <li>Bezbariérový přístup: {item.barier}</li>
+            <li>E-mail: {item.email}</li>
+            <li>Telefon: {item.phone}</li>
+          </ul>
+        ))}
+      </div>
+    </div>
+  );
 };
