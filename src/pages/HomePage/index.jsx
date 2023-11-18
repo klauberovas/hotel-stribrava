279444094000b06542b29588d7748956af62f8d5
Šárka Klauberová <sarkaa.novakova@seznam.cz>
2023-11-18 11:31:57 +0100

    components Rooms

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,11 +1,13 @@
 import './style.css';
 import { Header } from '../../components/Header';
+import { Rooms } from '../../components/Rooms';
 import { Footer } from '../../components/Footer';
 
 export const HomePage = () => {
   return (
     <>
       <Header />
+      <Rooms />
       <Footer />
     </>
   );
