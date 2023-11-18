06176d832fff317961375bf78e447dae8ff34ff6
Šárka Klauberová <sarkaa.novakova@seznam.cz>
2023-11-18 13:23:17 +0100

    doplnění formuláře

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -5,14 +5,18 @@ export const Form = () => {
     <form>
       <div className="form-fields">
         <label htmlFor="field1" className="field-label">
-          Datum:
+          Od:
         </label>
         <input id="field1" className="field-input" type="date" />
-
         <label htmlFor="field2" className="field-label">
+          Do:
+        </label>
+        <input id="field2" className="field-input" type="date" />
+
+        <label htmlFor="field3" className="field-label">
           Počet osob:
         </label>
-        <input id="field2" className="field-input" type="text" />
+        <input id="field3" className="field-input" type="text" />
 
         <label htmlFor="select" className="field-label">
           Stravování
@@ -28,8 +32,26 @@ export const Form = () => {
           Domácí mazlíček:
         </label>
         <input id="check1" className="field-input" type="checkbox" />
+        <label htmlFor="check2" className="field-label">
+          Přistýlka pro dítě:
+        </label>
+        <input id="check2" className="field-input" type="checkbox" />
+        <label htmlFor="check3" className="field-label">
+          Bezbariérový přístup:
+        </label>
+        <input id="check3" className="field-input" type="checkbox" />
+
+        <label htmlFor="field4" className="field-label">
+          E-mail:
+        </label>
+        <input id="field4" className="field-input" type="email" />
+
+        <label htmlFor="field5" className="field-label">
+          Mobil:
+        </label>
+        <input id="field5" className="field-input" type="number" />
       </div>
-      <button className="wide">Submit</button>
+      <button className="wide">Odeslat poptávku</button>
     </form>
   );
 };
