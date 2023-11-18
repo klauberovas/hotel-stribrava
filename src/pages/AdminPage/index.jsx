73c8f06da2fc6a44df6914f98dc3463f053d8ee8
Šárka Klauberová <sarkaa.novakova@seznam.cz>
2023-11-18 20:36:26 +0100

    order confirmation implemented

--- src/pages/AdminPage/index.jsx
+++ src/pages/AdminPage/index.jsx
@@ -4,24 +4,41 @@ import { useEffect, useState } from 'react';
 export const AdminPage = () => {
   const [orders, setOrders] = useState([]);
 
+  const fetchData = async () => {
+    const response = await fetch(
+      'http://localhost:4000/api/poptavky?filter=phase:eq:new',
+    );
+    const data = await response.json();
+    setOrders(data.result);
+  };
+
   useEffect(() => {
-    const fetchData = async () => {
-      const response = await fetch(
-        'http://localhost:4000/api/poptavky?filter=phase:eq:new',
-      );
-      const data = await response.json();
-      setOrders(data.result);
-    };
     fetchData();
   }, []);
-  console.log(orders);
+
+  const handleClick = async (state, id) => {
+    await fetch(`http://localhost:4000/api/poptavky/${id}`, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify([
+        {
+          op: 'replace',
+          path: '/phase',
+          value: state,
+        },
+      ]),
+    });
+    fetchData();
+  };
 
   return (
     <div className="container">
       <h1>Admin: Andulka</h1>
       <div className="list">
         {orders.map((item) => (
-          <ul className="list-item">
+          <ul key={item.id} className="list-item">
             <h3>Poptávka č.{item.id}</h3>
             <li>Počet nocí:{item.nights}</li>
             <li>Počet osob: {item.persons}</li>
@@ -30,8 +47,18 @@ export const AdminPage = () => {
             <li>Bezbariérový přístup: {item.barier}</li>
             <li>E-mail: {item.email}</li>
             <li>Telefon: {item.phone}</li>
-            <button className="button--confirm">❌</button>
-            <button className="button--confirm">✅</button>
+            <button
+              onClick={() => handleClick('rejected', item.id)}
+              className="button--confirm"
+            >
+              ❌
+            </button>
+            <button
+              onClick={() => handleClick('confirmed', item.id)}
+              className="button--confirm"
+            >
+              ✅
+            </button>
           </ul>
         ))}
       </div>
