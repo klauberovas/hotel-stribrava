cce2da3fba7e5ac6c6141373375dca2ff439fdb0
Šárka Klauberová <sarkaa.novakova@seznam.cz>
2023-11-18 11:01:28 +0100

    struktura, header

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,13 @@
+import './style.css';
+export const Header = () => {
+  return (
+    <div class="banner">
+      <div class="banner__stripe">
+        <div class="container">
+          <h1>Hotel Stříbrava</h1>
+          <p class="lead">Rodinný hotel v malebné krajině Českého středohoří</p>
+        </div>
+      </div>
+    </div>
+  );
+};
