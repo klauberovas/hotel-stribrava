4674d4b13ed29fd0c242f25b1b915fb8b7b4a741
Šárka Klauberová <sarkaa.novakova@seznam.cz>
2023-11-18 11:34:55 +0100

    component Room

---
+++ src/components/Room/index.jsx
@@ -0,0 +1,10 @@
+import './style.css';
+export const Room = () => {
+  return (
+    <div className="card">
+      <img className="card__image" src="img/image1.svg" />
+      <div className="card__title">Card 1</div>
+      <div className="card__body">Sunt natus</div>
+    </div>
+  );
+};
--- src/components/Rooms/index.jsx
+++ src/components/Rooms/index.jsx
@@ -1,3 +1,4 @@
+import { Room } from '../Room';
 export const Rooms = () => {
   return (
     <section className="dark">
@@ -5,11 +6,7 @@ export const Rooms = () => {
         <h2>Naše pokoje</h2>
         <p>Vyberte si, který z našich pokojů je pro vás ten pravý</p>
         <div className="cards-row">
-          <div className="card">
-            <img className="card__image" src="img/image1.svg" />
-            <div className="card__title">Card 1</div>
-            <div className="card__body">Sunt natus</div>
-          </div>
+          <Room />
         </div>
       </div>
     </section>
